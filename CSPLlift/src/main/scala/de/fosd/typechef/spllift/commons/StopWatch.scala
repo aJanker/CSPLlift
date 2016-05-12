7312b08ddc93322b7d633002221a7ad22cc3f2e8
Andreas Janker <an.janker@googlemail.com>
2016-05-12 09:55:22 +0200

    Improved Stopwatch

--- CSPLlift/src/main/scala/de/fosd/typechef/spllift/commons/StopWatch.scala
+++ CSPLlift/src/main/scala/de/fosd/typechef/spllift/commons/StopWatch.scala
@@ -8,7 +8,8 @@ import java.util.concurrent.{ConcurrentHashMap, TimeUnit}
 import scala.collection.JavaConverters._
 
 /*
- * A simple synchronized stopwatch implementation for measure the actual execution time of a given code block.
+ * A simple thread-safe stopwatch implementation for measuring the elapsed real (wall), user and system time
+ * for executing a given code block.
  *
  * Usage:
  *  val (duration, codeReturn) = StopWatch.measure("nameOfTheMeasurement", { some code })
@@ -21,28 +22,40 @@ object StopWatch {
 
     private def genId(): Int = this.synchronized {currentPeriodId += 1; currentPeriodId}
 
-    def measure[R](block: => R): (Long, R) = {
-        val t0 = bean.getCurrentThreadUserTime
+    def reset() = times.clear()
+
+    def measure[R](block: => R, takeTime: () => Long, convertToUnit : Long => Long = identity): (Long, R) = {
+        val t0 = takeTime()
         val result = block
-        val t1 = bean.getCurrentThreadUserTime
+        val t1 = takeTime()
 
-        (TimeUnit.NANOSECONDS.toMillis(t1 - t0), result)
+        (convertToUnit(t1 - t0), result)
     }
 
-    def measure[R](checkpoint: String, block: => R): (Long, R) = {
+    def measureCheckpoint[R](checkpoint: String, block: => R, takeTime: () => Long, convertToUnit : Long => Long = identity): (Long, R) = {
         val key = (genId(), checkpoint)
-        val (time, result) = measure(block)
-        times.put(key, time)
-        (time, result)
+        val (duration, result) = measure(block, takeTime, convertToUnit)
+        times.put(key, duration)
+
+        (duration, result)
     }
+
+    def measureSystemTime[R](block: => R): (Long, R) = measure(block, bean.getCurrentThreadCpuTime, TimeUnit.NANOSECONDS.toMillis)
+    def measureSystemTime[R](checkpoint: String, block: => R): (Long, R) = measureCheckpoint(checkpoint, block, bean.getCurrentThreadCpuTime, TimeUnit.NANOSECONDS.toMillis)
+
+    def measureUserTime[R](block: => R): (Long, R) = measure(block, bean.getCurrentThreadUserTime, TimeUnit.NANOSECONDS.toMillis)
+    def measureUserTime[R](checkpoint: String, block: => R): (Long, R) = measureCheckpoint(checkpoint, block, bean.getCurrentThreadUserTime, TimeUnit.NANOSECONDS.toMillis)
+
+    def measureWallTime[R](block: => R): (Long, R) = measure(block, System.currentTimeMillis)
+    def measureWallTime[R](checkpoint: String, block: => R): (Long, R) = measureCheckpoint(checkpoint, block, System.currentTimeMillis)
 
     def get(period: String): Long = times.asScala.find(_._1._2 equals period).map(_._2).getOrElse(-1)
 
     def getMeasurements: List[(String, Long)] = times.asScala.toList.sortBy(_._1._1).map(x => (x._1._2, x._2))
 
-    def toCSV: String = toCSV(";")
+    def toCSV: String = toCSV("; ")
     def toCSV(delimiter: String): String = toCSV(new StringWriter(), delimiter).toString
-    def toCSV(writer: Writer, delimiter: String = ";"): Writer = {
+    def toCSV(writer: Writer, delimiter: String = "; "): Writer = {
         val items = getMeasurements
         if (items.nonEmpty) {
             writer.append(items.map(_._1).mkString(delimiter))
@@ -54,8 +67,9 @@ object StopWatch {
 
     override def toString = print(new StringWriter()).toString
     def print(writer: Writer): Writer = {
+        def writeItem(w : Writer, item : (String, Long)) =  w.append("\n(" + item._1 + "\t" + item._2 + ")")
         val items = getMeasurements
         if (items.nonEmpty) writer.append("#stopwatch timings:\n")
-        items.foldLeft(writer) { (w, i) => w.append("\n(" + i._1 + "\t" + i._2 + ")") }
+        items.foldLeft(writer) { writeItem }
     }
 }
